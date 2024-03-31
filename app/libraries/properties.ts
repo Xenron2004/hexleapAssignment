bad77fd95145dd222bb2805ab01829d4547c6fda
Xenron2004 <kumarkoushal787075@gmail.com>
2024-04-01 00:11:56 +0530

    first commit

---
+++ app/libraries/properties.ts
@@ -0,0 +1,22 @@
+export type PlayerProps = {
+    playerName: string;
+    playerImage: string;
+    totalEvents: number;
+    sports: string;
+};
+
+export type CardProps = {
+    adImage: string;
+    adTitle: string;
+    adDesc: string;
+};
+
+export type SpotlightCardProps = {
+    title: string;
+    image: string;
+    date: string;
+    day: string;
+    time: string;
+    address: string;
+    collection: string;
+};
